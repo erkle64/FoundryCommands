16a188fc5f835c967c3012d070c8107c91a6d193
erkle64 <erkle64@gmail.com>
2022-06-02 20:30:13 +1200

    Initial source code commit

---
+++ FoundryCommands/FoundryCommands/FoundryCommandsLoader.cs
@@ -0,0 +1,57 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using UnhollowerRuntimeLib;
+using HarmonyLib;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace FoundryCommands
+{
+    [BepInPlugin(GUID, MODNAME, VERSION)]
+    public class FoundryCommandsLoader : BepInEx.IL2CPP.BasePlugin
+    {
+        public const string
+            MODNAME = "FoundryCommands",
+            AUTHOR = "erkle64",
+            GUID = "com." + AUTHOR + "." + MODNAME,
+            VERSION = "1.0.0";
+
+        public static BepInEx.Logging.ManualLogSource log;
+
+        public FoundryCommandsLoader()
+        {
+            log = Log;
+        }
+
+        public override void Load()
+        {
+            log.LogMessage("Registering PluginComponent in Il2Cpp");
+
+            try
+            {
+                ClassInjector.RegisterTypeInIl2Cpp<PluginComponent>();
+
+                var go = new GameObject("Erkle64_FoundryCommands_PluginObject");
+                go.AddComponent<PluginComponent>();
+                Object.DontDestroyOnLoad(go);
+            }
+            catch
+            {
+                log.LogError("FAILED to Register Il2Cpp Type: PluginComponent!");
+            }
+
+            try
+            {
+                var harmony = new Harmony(GUID);
+
+                var original = AccessTools.Method(typeof(ChatFrame), "onReturnCB");
+                var pre = AccessTools.Method(typeof(PluginComponent), "processChatEvent");
+                harmony.Patch(original, prefix: new HarmonyMethod(pre));
+            }
+            catch
+            {
+                log.LogError("Harmony - FAILED to Apply Patch's!");
+            }
+        }
+    }
+}
---
+++ FoundryCommands/FoundryCommands/PluginComponent.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using UnhollowerBaseLib;
+using HarmonyLib;
+using UnityEngine;
+
+namespace FoundryCommands
+{
+    public class PluginComponent : MonoBehaviour
+    {
+        public PluginComponent (IntPtr ptr) : base(ptr)
+        {
+        }
+
+        [HarmonyPrefix]
+        public static bool processChatEvent()
+        {
+            var message = ChatFrame.getMessage();
+            if(message.StartsWith("/tp "))
+            {
+                var wpName = message.Substring(4).ToLower();
+                if (wpName.Length > 0)
+                {
+                    var character = GameRoot.getClientCharacter();
+                    if (character != null)
+                    {
+                        foreach (var wp in character.getWaypointDict().Values)
+                        {
+                            if (wp.description.ToLower() == wpName)
+                            {
+                                GameRoot.addLockstepEvent(new GameRoot.ChatMessageEvent(character.usernameHash, string.Format("Teleporting to '{0}' at {1}, {2}, {3}", wp.description, wp.waypointPosition.x.ToString(), wp.waypointPosition.y.ToString(), wp.waypointPosition.z.ToString()), 0));
+                                GameRoot.addLockstepEvent(new Character.CharacterRelocateEvent(character.usernameHash, wp.waypointPosition.x, wp.waypointPosition.y, wp.waypointPosition.z));
+                                ChatFrame.hideMessageBox();
+                                return false;
+                            }
+                        }
+
+                        ChatFrame.addMessage(PoMgr._po("Waypoint not found."));
+                        ChatFrame.hideMessageBox();
+                        return false;
+                    }
+
+                    ChatFrame.addMessage(PoMgr._po("Client character not found."));
+                    ChatFrame.hideMessageBox();
+                    return false;
+                }
+
+                ChatFrame.addMessage(PoMgr._po("Usage: /tp <i>waypoint-name</i>"));
+                ChatFrame.hideMessageBox();
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
